d677f516f8ed08a29ddd5947376c81c2f5f95626
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-11-28 16:05:08 +0000

    TAASk4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: Prints exactly "Programming is like building a multilingual puzzle"
+ * Description: 
+ *		Prints exactly "Programming is like building a multilingual puzzle"
  *              followed by a new line using the puts function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual "
-         "puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
