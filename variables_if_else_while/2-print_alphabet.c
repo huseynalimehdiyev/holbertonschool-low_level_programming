5bdac6dae1b7c852c60e06e69a61cb44694ce18c
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-01 14:10:38 +0000

    TASK2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/** 
+* more headers goes there 
+*
+* Hello guys:
+*betty style doc for function main goes there 
+*/
+int main(void)
+{
+	char char1='a';
+	while (char1<='z')
+	{
+	    putchar("%c",char1);
+	    char1++;
+	    
+	}
+}
