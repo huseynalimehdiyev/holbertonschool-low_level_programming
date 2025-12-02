56d638f99ca552b26955e7ea10e3a0718f0c1ab5
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-02 17:59:23 +0000

    TASK7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the digits 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
