82e7a092f4cef4b87b7c88a7a8f3dcf5656ab633
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-02 17:48:33 +0000

    TASK5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, except q and e,
+ *        followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+	for (i=0; i<10; i++)
+	   printf("%d",i);
+    printf("\n");
+    
+	return (0);
+}
