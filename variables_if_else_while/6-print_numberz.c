ce169aaa4760c3918406929d053479b6ff9e126e
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-02 17:56:58 +0000

    TASk6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the digits 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
