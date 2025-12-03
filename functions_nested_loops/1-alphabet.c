bec03da969a67896b6c0437324af1188a7b1d856
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-03 19:56:48 +0000

    TASk1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,12 +2,15 @@
 
 /**
  * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
-	_putchar('\n');
+		_putchar(c);      /* 1-ci və yeganə döngə _putchar */
+
+	_putchar('\n');       /* 2-ci _putchar */
 }
