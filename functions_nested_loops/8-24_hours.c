b89cc9912a2ac4339d90494616948468be8258ec
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-03 20:57:13 +0000

    TASK8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,27 +1,4 @@
-Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
-
-Prototype: void jack_bauer(void);
-You can listen to this soundtrack while coding :)
-julien@ubuntu:~/$ cat 8-main.c
 #include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    jack_bauer();
-    return (0);
-}
-julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 8-main.c 8-24_hours.c -o 8-24
-julien@ubuntu:~/$ ./8-24 | head
-00:00
-00:01
-00:02
-00:03
-00:0#include "main.h"
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
