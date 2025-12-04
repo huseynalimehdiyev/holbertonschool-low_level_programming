c761311cfbc4c86cab5a7deffc77121b91e05251
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-04 20:32:41 +0000

    TASk8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using the # character
+ * @n: size of the square
+ */
+void print_square(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < n; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
