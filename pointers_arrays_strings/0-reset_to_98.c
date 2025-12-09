26e8b3f31756911b3c736d9ee0da400e30f6ef54
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-09 13:53:25 +0000

    TASK0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * reset_to_98 - updates the value of an integer to 98
  * @n: pointer to the integer to update
  *
  * Return: void
  */
-int main(void)
+void reset_to_98(int *n)
 {
-    int n;
-
-    n=402;
-    printf=("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	*n = 98;
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * reset_to_98 - updates the value pointed by a pointer to 98
- * @n: pointer to int
- */
 void reset_to_98(int *n);
 
-#endif /* MAIN_H */
+#endif
