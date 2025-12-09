7f5e0aa98fef4b299ef05b13f35b20537765bc6c
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-09 15:01:12 +0000

    TASK10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,28 +2,27 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: pointer to the string
+ * @s: pointer to string
  *
  * Return: integer value of the string
  */
 int _atoi(char *s)
 {
 	int i = 0, sign = 1, result = 0;
+	int started = 0;
 
-	/* Skip all characters until first digit or sign */
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			/* avoid overflow: just accumulate as int */
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (started)
 			break;
-		i++;
-	}
-
-	/* Convert digits to integer */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
