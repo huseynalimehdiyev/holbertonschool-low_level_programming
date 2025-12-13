6b11fdea0449761f41940bdb8e82ffe3d81feebb
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-13 15:14:49 +0400

    C TASKS

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,22 +13,17 @@ char *cap_string(char *s)
 
     while (s[i] != '\0')
     {
-        /* Capitalize first character if lowercase */
         if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-        {
             s[i] -= 32;
-        }
-
-        /* If current char is a separator, capitalize next char */
+			
         for (j = 0; separators[j] != '\0'; j++)
         {
             if (s[i] == separators[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-            {
                 s[i + 1] -= 32;
                 break;
-            }
         }
         i++;
     }
+
     return (s);
 }
