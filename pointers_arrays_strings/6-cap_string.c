644bac4c2e96423d1ad4dc57a88b3eea2b2a6a32
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-13 15:13:33 +0400

    C TASKS

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string
+ * cap_string - Capitalizes all words of a string
+ * @s: The string to modify
  *
- * Return: pointer to the modified string
+ * Return: A pointer to the modified string
  */
 char *cap_string(char *s)
 {
-	int i;
-	int sep;
+    int i = 0, j;
+    char separators[] = " \t\n,;.!?\"(){}";
 
-	char separators[] = " \t\n,;.!?\"(){}";
+    while (s[i] != '\0')
+    {
+        /* Capitalize first character if lowercase */
+        if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+        {
+            s[i] -= 32;
+        }
 
-	i = 0;
-	if (s[i] >= 'a' && s[i] <= 'z')
-		s[i] -= 32;
-
-	i = 1;
-	while (s[i] != '\0')
-	{
-		sep = 0;
-		for (int j = 0; separators[j] != '\0'; j++)
-		{
-			if (s[i - 1] == separators[j])
-				sep = 1;
-		}
-
-		if (sep && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-
-		i++;
-	}
-
-	return (s);
+        /* If current char is a separator, capitalize next char */
+        for (j = 0; separators[j] != '\0'; j++)
+        {
+            if (s[i] == separators[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+            {
+                s[i + 1] -= 32;
+                break;
+            }
+        }
+        i++;
+    }
+    return (s);
 }
