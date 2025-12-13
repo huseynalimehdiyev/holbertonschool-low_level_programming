e20a3cab101b7d5e44ee80c925fdb0a10325d015
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-13 15:45:27 +0400

    C TASKS

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,28 +2,36 @@
 
 /**
  * cap_string - Capitalizes all words of a string
- * @s: The string to modify
+ * @s: Pointer to the string to modify
  *
- * Return: A pointer to the modified string
+ * Description: Words are separated by space, tab, newline,
+ * comma, semicolon, period, exclamation mark, question mark,
+ * double quote, parenthesis, and curly braces.
+ *
+ * Return: Pointer to the modified string
  */
 char *cap_string(char *s)
 {
-    int i = 0, j;
-    char separators[] = " \t\n,;.!?\"(){}";
+	int i = 0, j;
+	char separators[] = " \t\n,;.!?\"(){}";
 
-    while (s[i] != '\0')
-    {
-        if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-            s[i] -= 32;
-			
-        for (j = 0; separators[j] != '\0'; j++)
-        {
-            if (s[i] == separators[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-                s[i + 1] -= 32;
-                break;
-        }
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		/* Capitalize first character if lowercase */
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
 
-    return (s);
+		/* If current char is a separator, capitalize next char */
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (s[i] == separators[j] &&
+			    (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+			{
+				s[i + 1] -= 32;
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
 }
