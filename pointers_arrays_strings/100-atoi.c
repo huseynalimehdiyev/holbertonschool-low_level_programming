15ecfb1b3752c66b798f0cff51f7f315814f0b7f
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-13 21:40:10 +0400

    C TASKS

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,9 +9,15 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	long result = 0; /* long istifadə edirik overflow yoxlamaq üçün */
+	int i;
+	int sign;
+	unsigned long acc;
+	unsigned long neg_limit;
+
+	i = 0;
+	sign = 1;
+	acc = 0;
+	neg_limit = (unsigned long)INT_MAX + 1UL;
 
 	/* handle signs before digits */
 	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
@@ -21,18 +27,20 @@ int _atoi(char *s)
 		i++;
 	}
 
-	/* build number */
+	/* build absolute value in acc (unsigned) */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + (s[i] - '0');
+		acc = acc * 10UL + (unsigned long)(s[i] - '0');
 
-		if (sign == 1 && result > INT_MAX)
+		if (sign == 1 && acc > (unsigned long)INT_MAX)
 			return (INT_MAX);
-		if (sign == -1 && -result < INT_MIN)
+		if (sign == -1 && acc > neg_limit)
 			return (INT_MIN);
 
 		i++;
 	}
 
-	return ((int)(result * sign));
+	if (sign == -1)
+		return ((int)(- (long)acc));
+	return ((int)acc);
 }
