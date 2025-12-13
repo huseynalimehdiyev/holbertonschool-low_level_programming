23a28b9605942e8ed3ab3c442877740c42fffb73
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-13 21:47:12 +0400

    C TASKS

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,43 +1,50 @@
 #include "main.h"
+#include <stdint.h>
 #include <limits.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: pointer to string
+ * _atoi - converts a string to an integer safely
+ * @s: string to convert
  *
- * Return: integer value
+ * Return: the converted integer
  */
 int _atoi(char *s)
 {
-	int i;
-	int sign;
-	int result;
-	int limit;
+	int sign = 1;
+	int found_digit = 0;
+	int64_t num = 0;
 
-	i = 0;
-	sign = 1;
-	result = 0;
-
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	/* Blank line after declarations */
+	while (*s)
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
+		if (*s == '-' && !found_digit)
+		{
+			sign = -sign;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			int digit = *s - '0';
+
+			/* Blank line after declaration */
+			found_digit = 1;
+			num = num * 10 + digit;
+		}
+		else if (found_digit)
+		{
+			break;
+		}
+
+		s++;
 	}
 
-	limit = (sign == -1) ? INT_MIN : -INT_MAX;
-
-	while (s[i] >= '0' && s[i] <= '9')
+	if (sign == 1 && num > INT_MAX)
 	{
-		if (result < (limit + (s[i] - '0')) / 10)
-			return (limit);
-
-		result = result * 10 - (s[i] - '0');
-		i++;
+		return (INT_MAX);
+	}
+	else if (sign == -1 && -num < INT_MIN)
+	{
+		return (INT_MIN);
 	}
 
-	if (sign == 1)
-		result = -result;
-
-	return (result);
+	return ((int)(sign * num));
 }
