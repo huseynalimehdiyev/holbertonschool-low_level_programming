613cfec776393ddc5b2251559ee041aef0dcb364
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-13 22:17:27 +0400

    C Tasks

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -28,4 +28,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _atoi(char *s);
 unsigned int _strspn(char *s, char *accept);
+void set_string(char **s, char *to);
 #endif
