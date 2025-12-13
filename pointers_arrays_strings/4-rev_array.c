3fd9480155c23a6bf0dcbec42adcb62efbc948d9
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-13 14:36:42 +0400

    C TASKS

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * reverse_array - reverses the content of an array of integers
+ * reverse_array - reverses the first n elements of an array
  * @a: array of integers
- * @n: number of elements in the array
+ * @n: number of elements to reverse
  */
 void reverse_array(int *a, int n)
 {
-    int i;
-    int tmp;
+	int i;
+	int tmp;
 
-    while (i < n / 2)
-    {
-        tmp = a[i];
-        a[i] = a[n - 1 - i];
-        a[n -1 -i] = tmp;
-        i++;
-    }
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
 }
