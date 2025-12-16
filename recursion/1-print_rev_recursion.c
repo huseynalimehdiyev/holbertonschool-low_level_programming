adb8b427eca5c2f15dd658ce413b6ddd9f1c0079
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-16 14:51:51 +0400

    C TASKS

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line
- * @s: string to be printed
+ * _print_rev_recursion - prints a string in reverse
+ * @s: pointer to the string to print
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-	{
-		_putchar('\n');
 		return;
-	}
 
-    _print_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
