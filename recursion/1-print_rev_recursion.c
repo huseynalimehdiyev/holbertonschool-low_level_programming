0846caacf6af54286ea28a5df105dd0609d337a2
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2025-12-16 14:50:22 +0400

    C TASKS

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -4,7 +4,7 @@
  * _puts_recursion - prints a string followed by a new line
  * @s: string to be printed
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
