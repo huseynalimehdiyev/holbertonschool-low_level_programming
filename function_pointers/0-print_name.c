f8ffbf934a111bac02e9c46a93361f3d81824927
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-01-03 00:13:33 +0400

    C taks

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name using a given function
+ * @name: name to print
+ * @f: pointer to a function that takes a string and returns void
+ *
+ * Return: Nothing
+ *
+ * Description: If f is not NULL, calls the function f with name as argument.
+ *              If f is NULL, does nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f)
+		return;
+
+	f(name);
+}
