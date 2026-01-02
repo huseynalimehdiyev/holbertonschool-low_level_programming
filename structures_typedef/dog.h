422091554fa44ac9a69fcbc10ca8c8305e4ea565
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-01-03 00:03:31 +0400

    C TASKS

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,5 +20,6 @@ typedef struct dog dog_t;
 /* Function prototypes */
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
