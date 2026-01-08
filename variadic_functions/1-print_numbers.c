a5f4c725d1e4ed256f653848513378ef0c0e184b
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-01-09 00:10:29 +0400

    python

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,19 +9,20 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
+	va_list args;
+	unsigned int i;
 
-    va_start(args, n);
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(args, int));
+	va_start(args, n);
 
-        if (separator != NULL && i != n - 1)
-            printf("%s", separator);
-    }
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
 
-    va_end(args);
-    printf("\n");
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
 
+	va_end(args);
+
+	printf("\n");
 }
