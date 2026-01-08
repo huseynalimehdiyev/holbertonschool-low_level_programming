1790aeda57b5aa53789aa60ca9437999bbf52f4d
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-01-08 23:54:53 +0400

    python

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 
@@ -10,19 +9,19 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    int sum = 0;
+	va_list args;
+	unsigned int i;
+	int sum = 0;
 
-    if (n == 0)
-        return (0);
+	if (n == 0)
+		return (0);
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-        sum += va_arg(args, int);
-    
-    va_end(args);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
 
-    return (sum);
+	va_end(args);
+
+	return (sum);
 }
