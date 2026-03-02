db492f1b68f92e558795307c4db09f9465faa1ac
huseynalimehdiyev <mehdiyevv.2007@gmail.com>
2026-03-02 23:12:22 +0400

    C TASKS

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the new hash table, or NULL if fails
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	ht->size = size;
+
+	return (ht);
+}
